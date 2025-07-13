c4c0abc7c94fc1d150a8fc5e8558c2b2f929cf6e
Kancharla Narmadha <119559316+kancharlaNarmadha@users.noreply.github.com>
2025-07-13 20:37:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getWeather() {
+  const city = document.getElementById("cityInput").value;
+
+  if (!city) {
+    document.getElementById("weatherBox").textContent = "Please enter a city name.";
+    return;
+  }
+
+  const apiKey = "YO15d64bbd4ef7d4a0fa1f122517242507"; 
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const location = data.location.name + ", " + data.location.country;
+    const temp = data.current.temp_c + "°C";
+    const condition = data.current.condition.text;
+
+    document.getElementById("weatherBox").innerHTML =
+      `<h3>${location}</h3>
+       <p><strong>Temperature:</strong> ${temp}</p>
+       <p><strong>Condition:</strong> ${condition}</p>`;
+  } catch (error) {
+    document.getElementById("weatherBox").textContent = "Could not fetch weather. Try again.";
+    console.log("Error:", error);
+  }
+}
